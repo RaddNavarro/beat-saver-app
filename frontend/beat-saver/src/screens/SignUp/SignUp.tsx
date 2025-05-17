1faf2b08220fb8e912e9c2007a7dfda0bde38d47
RaddNavarro <radian_navarro@dlsl.edu.ph>
2025-05-17 13:05:15 +0800

    update sign up

--- frontend/beat-saver/src/screens/SignUp/SignUp.tsx
+++ frontend/beat-saver/src/screens/SignUp/SignUp.tsx
@@ -9,14 +9,38 @@ import {
   InputText,
   SignUpText,
 } from "./SignUpStyles";
+import { useState } from "react";
+import { auth } from "../../db/firebase";
 
 const SignUp: React.FC<Props> = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async () => {
+    const res = await auth.createUserWithEmailAndPassword(email, password);
+
+    try {
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Container>
       <SignUpText>SignUp</SignUpText>
       <InputContainer>
-        <InputText placeholder="Email" />
-        <InputText placeholder="Password" secureTextEntry />
+        <InputText
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <InputText
+          placeholder="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry
+        />
       </InputContainer>
 
       <BtnContainer>
@@ -24,6 +48,7 @@ const SignUp: React.FC<Props> = ({ navigation }) => {
           <BtnText>Register</BtnText>
         </Btn>
       </BtnContainer>
+
       <Button title="Log In" onPress={() => navigation.navigate("LogIn")} />
     </Container>
   );
--- frontend/beat-saver/src/screens/SignUp/SignUpStyles.ts
+++ frontend/beat-saver/src/screens/SignUp/SignUpStyles.ts
@@ -5,7 +5,7 @@ export const Container = styled(View)`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: #fefefe;
+  background-color: rgb(245, 245, 245);
 `;
 
 export const SignUpText = styled(Text)`
@@ -14,19 +14,34 @@ export const SignUpText = styled(Text)`
 `;
 
 export const InputContainer = styled(View)`
-  width: "80%";
+  width: 80%;
 `;
 
 export const InputText = styled(TextInput)`
-  background-color: "white";
+  background-color: white;
   padding-horizontal: 15px;
   padding-vertical: 10px;
   border-radius: 10px;
   margin-top: 5px;
 `;
 
-export const BtnContainer = styled(View)``;
+export const BtnContainer = styled(View)`
+  width: 60%;
+  justify-content: center;
+  align-items: center;
+  margin-top: 40px;
+`;
 
-export const Btn = styled(TouchableOpacity)``;
+export const Btn = styled(TouchableOpacity)`
+  background-color: #0782f9;
+  width: 100%;
+  padding: 15px;
+  border-radius: 10px;
+  align-items: center;
+`;
 
-export const BtnText = styled(Text)``;
+export const BtnText = styled(Text)`
+  color: white;
+  font-weight: 700;
+  font-size: 16px;
+`;
