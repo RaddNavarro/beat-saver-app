8ef18b784cd58be6079af1c8d21501db83f2ffcb
JayM <jay_marlo_marmeto@dlsl.edu.ph>
2025-05-24 10:13:27 +0800

    Parent Screen Added

--- frontend/beat-saver/src/navigation/props.ts
+++ frontend/beat-saver/src/navigation/props.ts
@@ -84,3 +84,34 @@ export type PROPS = ModalProps & {
   isOpen: boolean;
   withInput?: boolean;
 };
+
+//parentHome.tsx types
+
+// teen profile
+export type Profile = {
+  id: string;
+  name: string;
+  image: string;
+  balance: number;
+};
+
+// teen expense
+export type Expense = {
+  id: string;
+  category: string;
+  description: string;
+  amount: number;
+  date: string;       // e.g., 'Friday', '2023-10-20'
+};
+
+// allowance request
+export interface AllowanceRequest {
+  id: string;
+  profileId: string;
+  reason: string;
+  amount: number;
+  status: 'pending' | 'approved' | 'denied';
+};
+
+// modals
+export type ModalType = 'profile' | 'allowance' | 'request';
